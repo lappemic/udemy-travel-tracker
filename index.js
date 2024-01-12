0f5f127d4f27d30b932ede9e1af803ec693c463c
Michael Lappert <mi.lappert@gmail.com>
2024-01-12 08:58:00 +0100

    update

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
+const db = new pg.Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'world',
+  password: 'WhyPostgres?',
+  port: 5432,
+});
+db.connect();
+
+app.get('/', async (req, res) => {
+  let visitedCountriesCodes = [];
+
+  db.query('SELECT country_code FROM visited_countries', (err, res) => {
+    if (err) {
+      console.log('Error executing query: ', err.stack);
+    } else {
+      const queryResult = res.rows;
+      console.log('query result: ', queryResult);
+
+      visitedCountriesCodes = queryResult.map(code => code.country_code);
+      console.log('visitedCountriesCodes: ', visitedCountriesCodes);
+    }
+  });
+
+  res.render('index.ejs', {
+    countries: visitedCountriesCodes,
+    total: visitedCountriesCodes.length,
+  });
 });
 
 app.listen(port, () => {
